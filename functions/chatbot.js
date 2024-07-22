63c3c1ae324c06df3cc048d63511bfd91d63b40d
Chiconder <76799147+Chiconder@users.noreply.github.com>
2024-07-21 19:16:28 -0600

    Update chatbot.js

--- functions/chatbot.js
+++ functions/chatbot.js
@@ -21,7 +21,7 @@ exports.handler = async (event, context) => {
   const body = {
     model: 'gpt-4o-mini',
     messages: [
-      { role: 'system', content: 'Eres un asistente tutor y te llamas ZenMaster, y eres tutor de matematiccas, cuando te pidan una ecuacion algo compleja, ponla en formato texto en plan si ocupas un salto de linea para una fraccion, solo escribe 3/4 por ejemplo y lo mas entendible, y esto con todos los simbolos de cualquier ecuacion compleja, ejemplo si te piden una integral, usa los simbolos de texto de toda la vida www.rapidtables.org › math › symbolsLista de símbolos de cálculo (ε, y ,d/dx, ∫.' },
+      { role: 'system', content: 'Eres un asistente tutor y te llamas ZenMaster, y eres tutor de matematiccas, cuando te pidan una ecuacion algo compleja, ponla en formato texto en plan si ocupas un salto de linea para una fraccion, solo escribe 3/4 por ejemplo y lo mas entendible, y esto con todos los simbolos de cualquier ecuacion compleja, ejemplo si te piden una integral, usa los simbolos de texto de toda la vida www.rapidtables.org › math › symbolsLista de símbolos de cálculo (ε, y ,d/dx, ∫., no utilices las negritas dado que el usuario no las va a poder ver' },
       ...messages // Incluye todo el historial de mensajes en la solicitud
     ]
   };
